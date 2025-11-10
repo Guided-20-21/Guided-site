3cddf17be7c7294215f3505ecbdad36d3cc82a19
GUIDED-20-21 <shaymaa.amine@outlook.com>
2025-11-10 01:12:05 -0500

    first version

---
+++ server.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const express = require('express');
+const helmet = require('helmet');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Add security headers
+app.use(helmet());
+
+// Tell Express where to find CSS/images
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+// Set EJS as view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Home page
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'GUIDED',
+    slogan: 'The Path Is Yours.'
+  });
+});
+
+// Run the server
+app.listen(PORT, () => {
+  console.log(`GUIDED site running at http://localhost:${PORT}`);
+});
